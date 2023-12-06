f7ab0f34e2425cce7b231a0b83ef476cbc945714
Myproject <mohamed.alaa@gmail.com>
2023-12-06 21:55:47 +0200

    add all color

--- lib/core/theming/app_colors.dart
+++ lib/core/theming/app_colors.dart
@@ -2,6 +2,10 @@ import 'package:flutter/material.dart';
 
 class AppColors {
   static const Color white = Color(0xffFFFFFF);
+  static const Color primary = Color(0xffFFD279);
   static const Color grey = Color(0xffB3B3B3);
   static const Color green = Color(0xff00957B);
+  static const Color textColor = Color(0xff484848);
+  static const Color brown = Color(0xffBA935C);
+  static const Color beige = Color(0xffBA935C);
 }
