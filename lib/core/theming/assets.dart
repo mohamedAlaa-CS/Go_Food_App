13d0ba1c9e8071cc91e0a338b63ab87f1d487a77
Myproject <mohamed.alaa@gmail.com>
2023-12-06 22:28:51 +0200

    add images

---
+++ lib/core/theming/assets.dart
@@ -0,0 +1,5 @@
+class Assets {
+  static const String background = 'assets/images/background.png';
+  static const String logo = 'assets/images/logo.png';
+  static const String testImage = 'assets/images/test_image.png';
+}
