0785a6ff3653b908b0e8673725e6343d60e7c06e
Myproject <mohamed.alaa@gmail.com>
2023-12-06 21:41:46 +0200

    app color

---
+++ lib/core/theming/app_colors.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color white = Color(0xffFFFFFF);
+  static const Color grey = Color(0xffB3B3B3);
+  static const Color green = Color(0xff00957B);
+}
