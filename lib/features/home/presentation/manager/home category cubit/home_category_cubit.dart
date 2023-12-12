8d3e0cb273d2196527e9b78844433cd3acca3755
Myproject <mohamed.alaa@gmail.com>
2023-12-12 22:41:50 +0200

    create fetch category method in cubit

--- lib/features/home/presentation/manager/home category cubit/home_category_cubit.dart
+++ lib/features/home/presentation/manager/home category cubit/home_category_cubit.dart
@@ -1,7 +1,25 @@
-import 'package:bloc/bloc.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:yjahz_app/features/home/data/models/home%20category%20model/home_category_model.dart';
+import 'package:yjahz_app/features/home/data/repos/home_repo.dart';
 
 part 'home_category_state.dart';
 
 class HomeCategoryCubit extends Cubit<HomeCategoryState> {
-  HomeCategoryCubit() : super(HomeCategoryInitial());
+  HomeCategoryCubit(this.homeRepo) : super(HomeCategoryInitial());
+  HomeCategoryCubit get(context) => BlocProvider.of(context);
+  final HomeRepo homeRepo;
+
+  //! fetch category Data
+  List<HomeCategoryModel> homeCategoryData = [];
+  fetchHomeCategory() async {
+    emit(HomeCategoryLoadingState());
+
+    var result = await homeRepo.fetchHomeCategory();
+    result.fold((failuer) {
+      emit(HomeCategoryFailuerState(failuer.message));
+    }, (categorylist) {
+      homeCategoryData.addAll(categorylist);
+      emit(HomeCategorySuccessState());
+    });
+  }
 }
